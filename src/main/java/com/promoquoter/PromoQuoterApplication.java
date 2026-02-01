99710fcfb8387d5573b86db71179650c6659697f
workuMekuriya <mekuriya185@gmail.com>
2026-02-01 14:05:43 +0300

    Project Setup
    - PromoQuoter cart pricing microservice

---
+++ src/main/java/com/promoquoter/PromoQuoterApplication.java
@@ -0,0 +1,12 @@
+package com.promoquoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoQuoterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoQuoterApplication.class, args);
+    }
+}
